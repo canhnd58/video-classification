6c2a08b1e47a4f56f50d4b28b16cdb3c09e0024f
hainam.le_internship <dealig29@gmail.com>
2016-12-01 18:55:36 +0700

    Update audio features extraction

--- src/audio.py
+++ src/audio.py
@@ -5,28 +5,23 @@ from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
 import matplotlib.pyplot as plt
 
-def extractAudioFeatures(videoPath):
-    # Extract audio from video
-    AUDIO_PATH = "videos/audio.wav"
-    command = "ffmpeg -i %s %s -y" % (videoPath, AUDIO_PATH)
-    FNULL = open(os.devnull, 'w')
-    subprocess.call(command, shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-
+def extractAudioFeatures(audioPath):
     # Extract features from audio file
-    [Fs, x] = audioBasicIO.readAudioFile(AUDIO_PATH)
+    [Fs, x] = audioBasicIO.readAudioFile(audioPath)
     x = audioBasicIO.stereo2mono(x)
-    # print "Total size:", len(x)
-    # print "Frame size:", Fs
     mF, sF = audioFeatureExtraction.mtFeatureExtraction(x, Fs, len(x), len(x), Fs, Fs)
-    return mF
+    res = list()
+    for item in mF:
+        res.append(item[0])
+    return res
 
 if __name__ == "__main__":
     if '--help' in sys.argv:
-        print "Usage:\tpython %s VIDEO_PATH" % sys.argv[0]
+        print "Usage:\tpython %s AUDIO_PATH" % sys.argv[0]
         sys.exit(0)
 
-    VIDEO_PATH = "videos/We-Dont-Talk-Anymore.mp4"
-    video = VIDEO_PATH if len(sys.argv) < 2 else sys.argv[1]
-    mF = extractAudioFeatures(video)
+    AUDIO_PATH = "videos/We-Dont-Talk-Anymore.wav"
+    audio = AUDIO_PATH if len(sys.argv) < 2 else sys.argv[1]
+    mF = extractAudioFeatures(audio)
     print "Mid-term features:"
     print mF
