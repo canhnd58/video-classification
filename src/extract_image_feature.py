3241428722c951af4cac1c65eb3dd70a3ad5bda5
thanhnc58 <thanhnc_58@vnu.edu.vn>
2016-12-02 22:20:45 +0700

    fixed image extraction

--- src/extract_image_feature.py
+++ src/extract_image_feature.py
@@ -2,39 +2,42 @@ import numpy as np
 import tensorflow as tf
 import copy
 import cv2
+import os
 from classify_image import*
 
 IMAGE_STEP = 30
-TOTAL_IMAGE = 10
+TOTAL_IMAGE = 4
 
-def split_video():
-  vidcap = cv2.VideoCapture('../videos/Doit.mp4')
+def split_video(video):
+  vidcap = cv2.VideoCapture(video)
   count = 0
   while count < IMAGE_STEP*TOTAL_IMAGE:
     success,image = vidcap.read()
     if count%IMAGE_STEP == 0:
-      temp = count/IMAGE_STEP + 1
+      temp = count/IMAGE_STEP
       cv2.imwrite("frame%d.jpg" % temp, image)     # save frame as JPEG file
     count += 1
 
-def extract_image_feature():
+def extract_image_feature(video):
   maybe_download_and_extract()
-  split_video()
+  split_video(video)
 
-  image = ('frame1.jpg')
+  image = ('frame0.jpg')
   feature = run_inference_on_image(image)
   
-  for num in range(2,TOTAL_IMAGE):
+  for num in range(1,TOTAL_IMAGE):
     image = ('frame%d.jpg' %num)
     temp = run_inference_on_image(image)
     feature = np.vstack((feature, temp))
   node_lookup = NodeLookup()
   feature = np.mean(feature,axis=0)
+  for num in range(0, TOTAL_IMAGE):
+  	os.system("rm " + 'frame%d.jpg' %num)
   return feature
 
 
 def main():
-  print(extract_image_feature())
+  print(extract_image_feature('../videos/Doit.mp4'))
 
 if __name__ == '__main__':
   main()
