619f58307d4ad3faab5047247564c8171bd92320
hainam.le_internship <dealig29@gmail.com>
2016-12-06 13:33:04 +0700

    Improve extract image features

--- src/extract_image_feature.py
+++ src/extract_image_feature.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 import copy
 import cv2
 import os
-from classify_image import*
+from classify_image import *
 
-IMAGE_STEP = 30
+# IMAGE_STEP * TOTAL_IMAGE should equals 720 (number of frames in 24fps 30s video)
+IMAGE_STEP = 360
 TOTAL_IMAGE = 2
 
 def split_video(video):
@@ -24,15 +25,17 @@ def extract_image_feature(video):
 
   image = ('frame0.jpg')
   feature = run_inference_on_image(image)
-  
+
   for num in range(1,TOTAL_IMAGE):
     image = ('frame%d.jpg' %num)
     temp = run_inference_on_image(image)
     feature = np.vstack((feature, temp))
-  node_lookup = NodeLookup()
+
+  # node_lookup = NodeLookup()
+
   feature = np.mean(feature,axis=0)
-  for num in range(0, TOTAL_IMAGE):
-  	os.system("rm " + 'frame%d.jpg' %num)
+  # for num in range(0, TOTAL_IMAGE):
+  # 	os.system("rm " + 'frame%d.jpg' %num)
   return feature
 
 
