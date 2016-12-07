e79e76ce104e25fa3089cb795b04a1c44b15b27e
hainam.le_internship <dealig29@gmail.com>
2016-12-07 15:20:59 +0700

    Improve processing images

--- src/process_image.py
+++ src/process_image.py
@@ -1,11 +1,32 @@
 import os
+import sys
 import subprocess
 
-count = 0
-for filename in os.listdir("videos"):
-    if filename.endswith(".avi"):
+if __name__ == '__main__':
+    if '--help' in sys.argv:
+        print "Usage:\tpython %s ID_TEXT_PATH NUMBER_TO_RUN" % sys.argv[0]
+        sys.exit(0)
+
+    id_path = sys.argv[1]
+    num = sys.argv[2]
+
+    count = 0
+    # Remove old image.csv file
+    fo = open("csv/image.csv", 'w')
+    fo.close()
+
+    fi = open(id_path, 'r')
+    contents = fi.readlines()
+
+    for content in contents:
+        if str(count) == num:
+            break
         count += 1
-        command = "python src/process_image_csv.py videos/%s" % filename
+        content = content[:-1]
+        if not os.path.exists("videos/%s.avi" % content):
+            print "videos/%s.avi not found! Continue... " % content
+            continue
+        command = "python src/process_image_csv.py videos/%s.avi" % content
         FNULL = open(os.devnull, 'w')
-        print "%s: Processing %s" % (count, filename)
-        subprocess.call(command, shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+        print "%s: Processing %s" % (count, content)
+        subprocess.call(command, shell=True, stdout=FNULL)
